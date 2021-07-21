6f2430cd7d708bfe34c1bcfc32691532680729c0
Dennis Salas <dgsl93.ds@gmail.com>
2021-07-20 19:21:43 -0500

    estilos a la pagina de inicio

--- src/Components/NavBarStem.jsx
+++ src/Components/NavBarStem.jsx
@@ -5,22 +5,27 @@ import { Link } from 'react-router-dom'
 const NavBarStem = () => {
     return (
         <div>
-            <Navbar>
+            <Navbar expand="lg">
                 <Container>
                     <Navbar.Brand >
-                        <Image src="https://i.ibb.co/9nvRxgM/Logo-proyecto.png" alt="logo" className="logo" roundedCircle/>
+                        <Image src="https://i.ibb.co/9nvRxgM/Logo-proyecto.png" alt="logo" className="logo" roundedCircle />
                     </Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Link className="nav-link" to="/">Inicio</Link>
-                        <Link className="nav-link" to="/Publications">Publicaciones</Link>
-                        <Link className="nav-link" to="/Services">Servicios</Link>
-                        <Link className="nav-link" to="/Stories">Historias</Link>
-                        <Link className="nav-link" to="/Contact">Contacto</Link>
-                        <Link className="nav-link"> 
-                        <Button className="btn-register me-3"> Crear cuenta</Button>
-                        <Button className="btn-login"> Acceder</Button>
-                        </Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Link className="nav-link" to="/">Inicio</Link>
+                            <Link className="nav-link" to="/Publications">Publicaciones</Link>
+                            <Link className="nav-link" to="/Services">Servicios</Link>
+                            <Link className="nav-link" to="/Stories">Historias</Link>
+                            <Link className="nav-link" to="/Contact">Contacto</Link>
+                            <Link className="nav-link">
+                                <Button className="btn-register me-3 btn"> Crear cuenta</Button>
+                            </Link>
+                            <Link className="nav-link">
+                                <Button className="btn-login btn"> Acceder</Button>
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -6,19 +6,23 @@ const App = () => {
     return (
         <>
             <Slider />
-            <Container>
-                <h1>STEM4U</h1>
-                <h4>Siempre Bienvenida!</h4>
-                <p>Si estas interesada en actualizarte, pertenecer, o conocer más sobre las areas de STEM.</p>
-                <h4>Te damos la bienvenida</h4>
-                <p>Somos un equipo que crea a través de la divulgación de información</p>
-                <p>Aqui vas a poder explorar, curiosear, aprender sobre lo nuevo en ciencia y tecnología</p>
-                <p>Todo lo que quieres saber sobre Ciencia, Tecnologia, Ingenieria y Matemáticas que no te habias atrevido a preguntar está aquí, dirigido a ti, de la forma en la que lo necesitas.</p>
-                <p>Entre, Explora, Perfilizate, aprende y has parte de la cuminidad global de mujeres en las areas de STEM.</p>
-                <h1>¿Que son las areas STEM?</h1>
-                <p>STEM es el acrónimo de Science, Technology, Engineering and Mathematics. Se trata de una integración de las ciencias en un enfoque de enseñanza basado en la interdisciplinaridad y aplicabilidad de los conocimientos de ciencias y matemáticas.</p>
-                <p>Los proyectos y actividades propuestos bajo esa finalidad tienen como objetivo la aplicación del conocimiento científico y matemático en un contexto vinculado a la tecnología y la ingeniería.
-                </p>
+            <Container className="my-3">
+                <h1 className="text-center">STEM4U Te da la bienvenida.</h1>
+                <p className="text-center"><em>"Somos un medio de centralización y divulgación de información de las areas STEM."</em></p>
+                <hr></hr>
+                <Container>
+                    <ul>
+                        <li>Mujer si estas interesada en pertenecer, actualizarte o conocer más sobre las areas de STEM.</li>
+                        <li>Aqui vas a poder explorar, informarte, aprender sobre lo nuevo en Ciencia, Tecnología, Matemática e Ingenieria y el impacto que las Mujeres tienen en estas áreas.</li>
+                        <li>Todo lo que quieres saber sobre Ciencia, Tecnologia, Ingenieria y Matemáticas que no te habias atrevido a preguntar está aquí, dirigido a ti, de la forma en la que lo necesitas.</li>
+                        <li>Entra, Registrate, Explora, Informate, Aprende y has parte de la cumunidad global de mujeres en las áreas de STEM.</li>
+                    </ul>
+                </Container>
+                <Container>
+                    <h1 className="text-center">¿Algunas se preguntarán que son las areas STEM?</h1>
+                    <p className="text-parrafo">STEM es el acrónimo de Science, Technology, Engineering and Mathematics. Se trata de una integración de las ciencias en un enfoque de enseñanza basado en la interdisciplinaridad y aplicabilidad de los conocimientos de ciencias y matemáticas. Los proyectos y actividades propuestos bajo esa finalidad tienen como objetivo la aplicación del conocimiento científico y matemático en un contexto vinculado a la tecnología y la ingeniería.
+                    </p>
+                </Container>
             </Container>
         </>
     )
