58e8b1c78cdee51a8f33ddf5d2f11cde754bd3cc
Dennis Salas <dgsl93.ds@gmail.com>
2021-07-24 15:45:49 -0500

    base de datos

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Form, Button, Container, Image } from "react-bootstrap";
 import { useForm } from "../hook/useForm";
 import "../style/loginS.css";
-import { startLoginEmailPassword, loginGoogle} from '../actions'
+import { startLoginEmailPassword, loginGoogle } from '../actions'
 import { useDispatch } from "react-redux";
 
 
 
 const Login = () => {
 
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
   const [formValues, handleInputChange] = useForm({
     email: "",
@@ -18,21 +18,20 @@ const Login = () => {
 
   const { email, password } = formValues;
 
-  const handleLogin = (e) =>
-  {
+  const handleLogin = (e) => {
     e.preventDefault();
     dispatch(startLoginEmailPassword(email, password))
     console.log(email, password);;
   }
 
-  
-  const handleGoogleLogin=()=>{
+
+  const handleGoogleLogin = () => {
     dispatch(loginGoogle());
     console.log(dispatch(loginGoogle()))
-   }
+  }
   return (
     <div className="login-root">
-      <Form onSubmit={handleLogin}  className="form-login">
+      <Form onSubmit={handleLogin} className="form-login">
         <Container>
           <Image
             src="https://i.ibb.co/9nvRxgM/Logo-proyecto.png"
@@ -43,38 +42,38 @@ const Login = () => {
         </Container>
         <Form.Group className="mb-3 " controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control 
-          type="email" 
-          name= "email"
-          placeholder="Enter email" 
-          value = {email}
-          onChange= {handleInputChange}
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={handleInputChange}
           />
-          
+
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control 
-          type="password" 
-          name ="password"
-          placeholder="Password" 
-          value = {password}
-          onChange ={handleInputChange}
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={handleInputChange}
           />
-          
+
         </Form.Group>
         <Button className="btn" type="submit">
           Iniciar Sesion
         </Button>
-        <Container className="auth__social-networks">
-          <Container
-            className="google-btn">
-            <Container className="google-icon-wrapper">
-              <img className="google-btn" onClick={handleGoogleLogin} src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
-            </Container>
-          </Container>
-        </Container>
+        <Container className="auth__social-networks">
+          <Container
+            className="google-btn">
+            <Container className="google-icon-wrapper">
+              <img className="google-btn" onClick={handleGoogleLogin} src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+            </Container>
+          </Container>
+        </Container>
       </Form>
     </div>
   );
