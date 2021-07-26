0671c02bf9a83b1e56a568afab3dd7d5bd764711
Henry <luisgasthof@hotmail.com>
2021-07-25 21:11:25 -0500

    adicion de formulario de contacto

--- src/Containers/Contact.jsx
+++ src/Containers/Contact.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import "../style/contact.css";
 
 export const Contact = () => {
-    return (
-        <div>
-            <h1>Contacto</h1>
-        </div>
-    )
-}
-
-
+  return (
+    <div>
+      <div className="contact-clean">
+        <form method="post">
+          <h2 className="text-center">Contact us</h2>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              placeholder="Nombre"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              className="form-control"
+              rows="14"
+              name="message"
+              placeholder="Mensaje"
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              send
+            </button>
+          </div>
+        </form>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    </div>
+  );
+};
