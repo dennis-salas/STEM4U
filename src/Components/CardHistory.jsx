39ad4d2dce024649f94fe8f3082c6218718a5e51
Dennis Salas <dgsl93.ds@gmail.com>
2021-07-26 08:55:59 -0500

    Modelos de card de historias

---
+++ src/Components/CardHistory.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import { Card, Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
+const CardHistory = () => {
+
+    const [women, setWomen] = useState([])
+
+    useEffect(() => {
+
+        axios.get(`http://localhost:3004/History`)
+            .then(res => {
+                const listWomen = res.data;
+                setWomen(listWomen);
+            })
+
+    }, [])
+
+    return (
+        <Container>
+            {
+                women.map(ele => (
+                    <Card className="my-5" key={ele.id}>
+                        <Row className="justify-content-md-center align-items-center">
+                            <Col md={4} sm={12}>
+                                <Card.Img variant="top" src={ele.image} fluid="true" />
+                            </Col>
+                            <Col md={8} sm={12}>
+                                <Card.Body>
+                                    <Card.Title>{ele.name}</Card.Title>
+                                    <Card.Text><small className="text-muted">
+                                        {ele.contribution}
+                                    </small></Card.Text>
+                                </Card.Body>
+                            </Col>
+                        </Row>
+                    </Card>
+                ))
+            }
+        </Container>
+    )
+}
+
+export default CardHistory
--- src/Components/NavBarStem.jsx
+++ src/Components/NavBarStem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Container, Nav, Image, Button, Row, Col } from 'react-bootstrap'
+import { Navbar, Container, Nav, Image, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 
