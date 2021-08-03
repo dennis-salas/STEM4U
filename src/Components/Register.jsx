f9610f80548a204fe025d5f206c51b1a938f8f21
Henry <luisgasthof@hotmail.com>
2021-08-03 11:45:32 -0500

    imagen en contacto

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Button, Form, Container, Image } from 'react-bootstrap'
-import '../style/loginS.css';
-import { useForm } from '../hook/useForm';
-import { useDispatch } from 'react-redux';
-import {registroUsuario} from '../action/actions'
-
-
+import React from "react";
+import { Button, Form, Container, Image } from "react-bootstrap";
+import "../style/loginS.css";
+import { useForm } from "../hook/useForm";
+import { useDispatch } from "react-redux";
+import { registroUsuario } from "../action/actions";
 
 const Register = () => {
-   
   const dispatch = useDispatch();
 
   const [formValues, handleInputChange, reset] = useForm({
     nombres: "",
     apellidos: "",
     email: "",
-    password: ""
+    password: "",
   });
 
   const { nombres, apellidos, email, password } = formValues;
@@ -33,21 +30,52 @@ const Register = () => {
         <div className="login-root">
           <Form className="form-login" onSubmit={handleRegistro}>
             <Container className="row justify-content-center">
-              <Image src="https://i.ibb.co/9nvRxgM/Logo-proyecto.png" alt="logo" className="logo" roundedCircle />
+              <Image
+                src="https://i.ibb.co/9nvRxgM/Logo-proyecto.png"
+                alt="logo"
+                className="logo"
+                roundedCircle
+              />
             </Container>
             <Form.Group className="mb-3 " controlId="formBasicName">
               <Form.Label>Nombres</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" name="nombres" value={nombres} onChange={handleInputChange}/>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                name="nombres"
+                value={nombres}
+                onChange={handleInputChange}
+              />
             </Form.Group>
             <Form.Group className="mb-3 " controlId="formBasicEmail">
               <Form.Label>Apellidos</Form.Label>
-              <Form.Control type="text" placeholder="Enter lastname" name="apellidos"value={apellidos} onChange={handleInputChange} />
-            </Form.Group><Form.Group className="mb-3 " controlId="formBasicLastName">
+              <Form.Control
+                type="text"
+                placeholder="Enter lastname"
+                name="apellidos"
+                value={apellidos}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 " controlId="formBasicLastName">
               <Form.Label>Correo electronico</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" name="email" value={email} onChange={handleInputChange}/>
-            </Form.Group><Form.Group className="mb-3 ">
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3 ">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" name="password" placeholder="Enter password" value={password} onChange={handleInputChange} />
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={handleInputChange}
+              />
             </Form.Group>
             <Button className="btn" type="submit">
               Enviar
@@ -56,7 +84,7 @@ const Register = () => {
         </div>
       </Container>
     </Container>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- src/Containers/Contact.jsx
+++ src/Containers/Contact.jsx
@@ -4,40 +4,47 @@ import "../style/contact.css";
 export const Contact = () => {
   return (
     <div>
-      <div className="contact-clean">
-        <form method="post">
-          <h2 className="text-center">Contáctanos</h2>
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="text"
-              name="name"
-              placeholder="Nombre"
-            />
-          </div>
-          <div className="form-group">
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="form-group">
-            <textarea
-              className="form-control"
-              rows="14"
-              name="message"
-              placeholder="Mensaje"
-            ></textarea>
-          </div>
-          <div className="form-group">
-            <button className="btn" type="submit">
-              Enviar
-            </button>
-          </div>
-        </form>
+      <div className="row contact-clean">
+        <div className="col-sm-3 ms-5">
+          <img className="col-sm" src="https://res.cloudinary.com/dusnjqou6/image/upload/v1627928006/Womanpower_logo_ru24fc.svg" width="300px" height="405" alt="imagen" />
+        </div>
+        <div className="col-sm-6">
+          <div class="d-none d-md-flex col-md-4 col-lg-6 "></div>
+          <form method="post">
+            <h2 className="text-center">Contáctanos</h2>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder="Nombre"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                className="form-control"
+                rows="14"
+                name="message"
+                placeholder="Mensaje"
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <button className="btn" type="submit">
+                Enviar
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
+
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     </div>
