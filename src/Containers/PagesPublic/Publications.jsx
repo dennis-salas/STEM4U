58b68d2d00fa183ee8ba8201010c844b14b60a9e
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-08-04 17:26:50 -0500

    corrigiendo merge

--- src/Containers/PagesPublic/Publications.jsx
+++ src/Containers/PagesPublic/Publications.jsx
@@ -1,34 +1,9 @@
-<<<<<<< HEAD
-import React from "react";
-import axios from "axios";
-import CardPublic from "../../Components/CardPublic";
-import { useEffect, useState } from "react";
-=======
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
->>>>>>> 38f261ba113c79c8559853dd1eaa93a28b61bc4d
 
 export const Publications = ({}) => {
   const [noticias, setNoticias] = useState([]);
-
   useEffect(() => {
-<<<<<<< HEAD
-    axios.get(
-      `https://newsapi.org/v2/top-headlines/sources?apiKey=73ad4b9c8b6b41ea83873ed4eeb61081&category=technology&category=science`
-    ).then((response) => {
-      const listNoticias = response.data
-      setNoticias(listNoticias);
-      console.log(noticias);
-    });
-  },[]);
-   
-  return (
-    <div>
-     <h1>hola</h1>
-  </div>
-  );
-};
-=======
     axios.get(`https://newsapi.org/v2/top-headlines/sources?apiKey=73ad4b9c8b6b41ea83873ed4eeb61081&category=technology&category=science`)
       .then(res => {
         const listNoticias = res.data.sources;
@@ -58,4 +33,3 @@ export const Publications = ({}) => {
 
 
 
->>>>>>> 38f261ba113c79c8559853dd1eaa93a28b61bc4d
