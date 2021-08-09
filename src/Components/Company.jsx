6ee71e1cecdab85a5195305d1fa7644ccfce8f64
Dennis Salas <dgsl93.ds@gmail.com>
2021-08-09 18:55:48 -0500

    Puntos claves completo

---
+++ src/Components/Company.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Container, Col, Row } from 'react-bootstrap'
+
+const Company = () => {
+    return (
+        <Container className="my-4">
+            <Row>
+                <Col md={3} sm={6}>
+                    <img className="mx-auto d-block img-fluid img-app" src="https://i.ibb.co/1G05hXj/company.jpg" alt="imagen" />
+                </Col>
+                <Col md={9} sm={6}>
+                    <h1>Investigación Empresarial</h1>
+                    <p className="text-parrafo">Alguna de las cualidades más valoradas en el mundo empresarial son la fidelidad y el compromiso que tiene un empleado con los objetivos, metas y desarrollo de la empresa, donde labore, por esa razon es necesario que realices una investigación sobre la empresa en que vas  postularte, los aspectos mas releantes de la empresa como misión, visión, valores, directivos actuales entre otros, los aspectos externos de la empresa coo su posicionamiento comercial, competencias, ventas realizadas o fluctuación de su economia, obeten est información por medio de herramientas como los motores de busquedas de internet o una red de contactos confiable para asegurar la veracidad de la información, al mencionar estos datos en una entrevista lograras transmitir tu interes en ser parte de su equipo de trabajo mostrando el compromiso y fidelidad que podrias ofrecer a la empresa</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h3>Herramientas de investigación</h3>
+                    <p className="text-parrafo">Es indispensable que utilices las herramientas correctas para investigar a la empresa con información confiable. Los buscadores son las herramientas principales para localizar páginas, documentos, noticias, imágenes o videos en internet. Para utilizarlos, sólo debes ingresar palabras clave de la información que necesitas en la caja de búsqueda y analizar los resultados arrojados. Toma en cuenta que:</p>
+                    <ul>
+                        <li>Estos están ordenados por grado de relevancia.</li>
+                        <li>La caja de búsqueda cuenta con sugerencias que te facilitan el trabajo.</li>
+                        <li>No todos los resultados estarán verdaderamente relacionados con el tema que investigas, por lo que debes analizarlos.</li>
+                        <li>En la misma búsqueda puedes encontrar sitios, imágenes, videos, mapas y noticias.</li>
+                        <li>Los resultados pueden filtrarse por región, idioma, fecha, tamaño, y otros parámetros.</li>
+                        <li>Puedes obtenerse más resultados si pruebas con palabras claves.</li>
+                        <li>Existen buscadores especializados que arrojan resultados sobre un mismo tema.</li>
+                        <li>El historial de busquedas puede influir en los resultados que obtengas, para evitarlo, activa la opción modo incognito.</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6} sm={6}>
+                    <h3>¿Que buscar?</h3>
+                    <p>La información más relevante que puedes encontrar en internet sobre una empresa es:</p>
+                    <ul>
+                        <li>Declaración de objetivos</li>
+                        <li>Informes anuales</li>
+                        <li>Bienes y servicios que ofrecen</li>
+                        <li>Clientes</li>
+                        <li>Preguntas frecuentes</li>
+                        <li>Programas que patrocinan la empresa</li>
+                    </ul>
+                </Col>
+                <Col md={6} sm={6}>
+                    <h3>Red de contactos</h3>
+                    <p className="text-parrafo">Las personas que trabajan o que están familiarizadas con la empresa, incluso sus competidores, son fuentes con información que no podrás encontrar en internet. Éstas pueden compartir contigo:</p>
+                    <ul>
+                        <li>Las últimas novedades</li>
+                        <li>Tendencias</li>
+                        <li>Actualizaciones</li>
+                        <li>Metas de la empresa</li>
+                    </ul>
+                </Col>
+            </Row>
+            <Row>
+                <h3>Investigación interna de la empresa</h3>
+                <p className="text-parrafo">La investigación interna de la empresa esta constituida por la misión, visión, personas que la conforman y otros aspectos relevantes, para realizar una investigación de la empresa, informate sobre su giro, historia y actividades en su página web, esta información se encuentra regularmente en la sección acerca de nosotros, identifica el perfil de las vacantes publicadas para conocer las habilidades que requieren, puedes conseguir esta información en su bolsa de trabajo aunque no todas las empresas ceuntan con una, investiga sobre el ambiente de trabajo, puedes consultar ranking en diversos sitios y publicaciones o en la redes sociales, busca noricas de la compañia asi podras infromarte sobre la rotación se los ejecutivos, lanzamientos de nueos prodductos, premios que se le hallan otrogado y reportes de ingresos.</p>
+            </Row>
+        </Container>
+    )
+}
+
+export default Company
---
+++ src/Components/LinkedIn.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Container, Col, Row, Image } from 'react-bootstrap'
+
+const LinkedIn = () => {
+    return (
+        <Container className="my-4">
+            <Row>
+                <Col md={8} sm={6}>
+                    <h1>LinkedIn</h1>
+                    <hr />
+                    <p className="text-parrafo">LinkedIn es una red social que nace con el objetivo de poner en contacto a empresas y profesionales que buscan sinergias laborales y nuevas oportunidades de negocio, mediante una red de relaciones comerciales denominadas “conexiones”. Esta red cuenta con una gran comunidad social, muy útil para las empresas, negocios que buscan nuevo talento humano, y por supuesto, para las personas que se encuentran en búsqueda de un empleo. Todo esto gracias a la introducción de fichas personales de cada persona a modo de currículum vitae, donde se comparte la formación y experiencia laboral.</p>
+                </Col>
+                <Col md={4} sm={6}>
+                    <Image src="https://i.ibb.co/zZqhQG6/linkendin.jpg" alt="logo" className="mx-auto d-block img-fluid img-app" />
+                </Col>
+            </Row>
+            <Row>
+                <Col md={4} sm={6}>
+                    <Image src="https://i.ibb.co/74xGXkn/shutterstock-301955957.jpg" alt="logolinkedin" className="mx-auto d-block img-fluid img-app" />
+                </Col>
+                <Col md={8} sm={6}>
+                    <h3>¿Para qué sirve LinkedIn?</h3>
+                    <hr />
+                    <p className="text-parrafo">Ahora que conoces qué es LinkedIn, es hora de hablar de para qué sirve realmente esta plataforma. Al tratarse de una red social profesional muy distinta a las demás, sus usos poco o nada tienen que ver con el resto, como puedes suponer. Dentro de esta plataforma profesional, los contenidos de ocio y entretenimiento no tienen cabida, por lo que se deben dejar a un lado. En cambio, se deben de crear contenidos orientados al ámbito laboral, profesional y empresarial. Desde el mismo instante en el que se decide hacer parte de la plataforma, se debe entender que todo el contenido que allí se publique, debe reflejar un estilo formal y profesional, de lo contrario, se estaría comprometiendo la reputación dentro de la plataforma, y no la tomarán en serio, ya sea sí se está en la búsqueda de empleo o de nuevos colaboradores. Dejando todo esto claro, es momento de desvelarte para qué sirve LinkedIn realmente.</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h3>Encontrar empleo con LinkedIn</h3>
+                    <hr />
+
+                    <p className="text-parrafo">El principal uso de LinkedIn es ayudarte en la búsqueda de empleo. Cuando abres una cuenta de perfil por primera vez, debes de diligenciar todos los campos del formulario. Esto incluye tu experiencia profesional y laboral, estudios, certificados, habilidades, documentos y demás. Las cuentas de perfil en LinkedIn funcionan igual que una hoja de vida. Por ello, es muy importante que te tomes el tiempo para redactarlo de forma apropiada. Esta red social es sumamente activa, por lo que día a día una gran cantidad de reclutadores ingresan en la búsqueda de nuevos talentos para las empresas.</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h3>Consejos para tener un perfil de LinkedIn perfecto</h3>
+                    <hr />
+                    <ol>
+                        <li className="text-parrafo">Actualiza tu perfil profesional: Si tu objetivo es que te encuentren, mejorar tu marca personal o el poder acceder a nuevas oportunidades laborales, debes mantener tu perfil de usuario actualizado con tus últimos datos de experiencia laboral, habilidades obtenidas, empleos, aptitudes que has desarrollado y demás.</li>
+                        <li className="text-parrafo">Utiliza una fotografía profesional: Si tu cuenta de LinkedIn cuenta con una buena foto de perfil, aumentarán tus posibilidades de sobre salir frente aquellos perfiles que no tienen una fotografía o son de mala calidad.</li>
+                        <li className="text-parrafo">Utiliza un baner llamativo: Donde destaques tus habilidades profesinales.</li>
+                        <li className="text-parrafo">Incluye palabras clave: Debes incluir palabras clave en tu perfil con el objetivo de que te localicen. Estas palabras clave deben estar relacionadas con tu perfil profesional y tus aptitudes, de modo tal que, la información que suministres sea clara y orientada a tu vida profesional.</li>
+                        <li className="text-parrafo">Crea una URL personalizada: Una URL personalizada hará que tu perfil de LinkedIn se vea aún más profesional. Por lo general, las Url de los perfiles son una serie de números. Sin embargo, esta se puede reemplazar, por ejemplo, por tu nombre y apellido. De tal modo que al acceder a tu perfil de LinkedIn, se muestre la URL de la siguiente forma: https://linkedin.com/nombre-apellido/.</li>
+                        <li className="text-parrafo">Consigue referencias y recomendaciones: Al igual que pasa en la vida real, las recomendaciones son sumamente importantes para tu perfil de LinkedIn. Es importante lograr conseguir recomendaciones de anteriores empleos e incluso de la Universidad o Instituto, para poderlas incluir dentro de tu cuenta, así logras resaltar tu trayectoria y conseguir validarla con otros especialistas de tu rama. Estas referencias permitirán a los empleadores o profesionales de talento humano en búsqueda de personal, el poder validar tu perfil.</li>
+                        <li className="text-parrafo">Conecta con otros profesionales del sector: LinkedIn ofrece la posibilidad de poder conectar con otras personas del sector dentro de la red social con el objetivo de aumentar tu red de contactos. Aprovecha esta función sabiamente y no abuses de las invitaciones y mensajes privados. Puedes enviar solicitudes a otros profesionales que se encuentren dentro de tu área, antiguos compañeros de trabajo o estudios, personal de talento humano, directores de compañías e incluso, con otros profesionales con los que tengas interés de conectar y añadir a tus contactos.</li>
+                        <li className="text-parrafo">Incluye un titular profesional atractivo: El titular es uno de los elementos al que mayor atención se le presta al momento de evaluar tu perfil, por ello, debes de crear un titular atractivo. Te recomendamos seguir estos pasos para lograr conseguir un titular atractivo:
+                            <ul>
+                                <li>Añade cualidades dentro del cuenta (Experiencia en SEO, experiencia en marketing, conocimiento en un idioma, entre otras cualidades).</li>
+                                <li>Evita los errores ortográficos dentro del titular.</li>
+                                <li>Utiliza mayúsculas en los lugares que corresponda (La mayúscula sostenida puede ser interpretada como que estás gritando).</li>
+                                <li>Ten cuidado con el uso de siglas que no se conocen.</li>
+                                <li>No es necesario abarcar todo el espacio, se puede decir mucho con pocas palabras.</li>
+                                <li>Es mejor mostrar tu experiencia que usar la palabra “experto” dentro del título. Diversos estudios afirman que esta palabra dice muy poco de ti.</li>
+                                <li>Procura encajar más palabras clave, usando más contenido al momento de añadir el título.</li>
+                            </ul>
+                        </li>
+                        <li className="text-parrafo">Destaca tu experiencia, logros y habilidades: Si logras resaltar adecuadamente tu experiencia, tus logros y las habilidades obtenidas, marcarás la diferencia entre tu perfil profesional y del de otra persona. Puedes hacer más atractiva tu marca personal añadiendo secciones que den a conocer tu experiencia, logros y habilidades mediante la opción “Editar perfil”. Añade las que consideres necesarias, sin llegar a saturar tu perfil.</li>
+                        <li className="text-parrafo">Participa en grupos del sector: LinkedIn permite a sus usuarios participar en grupos especializados en temáticas específicas, grupos de oferta de empleo y demás grupos que permitan mejorar tu red de contactos. Participa en los que vayan acorde a tu perfil o lo que estés buscando. Sé activo en ellos para aumentar la visibilidad de tu perfil en la plataforma. Con ello además, podrás conseguir nuevos contactos.</li>
+                    </ol>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default LinkedIn
