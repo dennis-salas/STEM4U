688bc4f8deede8829be9039cdc5f9baf901422d4
Dennis Salas <dgsl93.ds@gmail.com>
2021-08-11 19:49:03 -0500

    bd de capacitaciones

--- src/Containers/PagesPrivate/Training.jsx
+++ src/Containers/PagesPrivate/Training.jsx
@@ -1,67 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Header from '../../Components/Header'
 import '../../style/services.css'
+import axios from "axios";
+import { Card, Form } from "react-bootstrap";
 
 const Training = () => {
+
+    const [categories, setCategories] = useState([]);
+    const [formValue, setFormValue] = useState({})
+
+    useEffect(() => {
+        axios.get(`http://localhost:3004/TrainingCategories`).then((res) => {
+            const listCategories = res.data;
+            setCategories(listCategories);
+        });
+
+    }, []);
+
+    const handleInputChange = ({ target }) => {
+        setFormValue({
+            ...formValue,
+            [target.name]: target.value
+        });
+    }
+    console.log(formValue)
+
     return (
         <div>
-            <Header/>
+            <Header />
             <h1>Capacitación</h1>
-            <div class="row services ms-5" >
-                <div class="col-sm-4">
+            <div className="row services ms-5" >
+                <div className="col-sm-4">
                     <h1>Seleccione el área:</h1>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                        <label class="custom-control-label" for="customCheck1">Ciencias</label>
-                    </div>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                        <label class="custom-control-label" for="customCheck1">Tecnologias</label>
-                    </div>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                        <label class="custom-control-label" for="customCheck1">Ingenieria</label>
-                    </div>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="customCheck1" />
-                        <label class="custom-control-label" for="customCheck1">Matematicas</label>
-                    </div>
-
+                    <Form >
+                        <div onChange={handleInputChange}>
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" name="ciencias" value="ciencias" />
+                                <label className="custom-control-label" for="customCheck1">Ciencias</label>
+                            </div>
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" name="tecnologia" value="tecnologia" />
+                                <label className="custom-control-label" for="customCheck1">Tecnologia</label>
+                            </div>
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" name="ingenieria" value="ingenieria" />
+                                <label className="custom-control-label" for="customCheck1">Ingeniería</label>
+                            </div>
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" name="matematicas" value="matematicas" />
+                                <label className="custom-control-label" for="customCheck1">Matemáticas</label>
+                            </div>
+                        </div>
+                    </Form>
                 </div>
-                <div class="col-sm-6">
-                    <div class="container">
-                        <div class="row col-xs-12">
-                            <div class="card">
-                                <img class="card-img-top" src="https://www.gob.mx/cms/uploads/image/file/469900/blog_CAPACITACI_N.png" alt="Card image cap" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Matematicas</h5>
-                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                </div>
-                            </div>
-                            <div class="card">
-                                <img class="card-img-top" src="https://www.gob.mx/cms/uploads/image/file/469900/blog_CAPACITACI_N.png" alt="Card image cap" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Tecnologias</h5>
-                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                </div>
-                            </div>
-                            <div class="card">
-                                <img class="card-img-top" src="https://www.gob.mx/cms/uploads/image/file/469900/blog_CAPACITACI_N.png" alt="Card image cap" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Ingenieria</h5>
-                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                                </div>
-                            </div>
-                            <div class="card">
-                                <img class="card-img-top" src="https://www.gob.mx/cms/uploads/image/file/469900/blog_CAPACITACI_N.png" alt="Card image cap" />
-                                <div class="card-body">
-                                    <h5 class="card-title">Ciencias</h5>
-                                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                {
+                    categories.map(ele => (
+                        <div className="col-sm-6" key={ele.id}>
+                            <div className="container">
+                                <div className="row col-xs-12">
+                                    <div className="card">
+                                        <img className="card-img-top img-fluid" src={ele.img} alt="CardImage" />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{ele.category}</h5>
+                                            <p className="card-text">{ele.name}</p>
+                                            <Card.Link target="_blank" href={ele.enlace}>Ver mas</Card.Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    ))
+                }
             </div>
         </div>
     )
