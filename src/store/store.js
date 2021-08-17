d91b641620f4b09137ff1ec66417bcbb3642a82a
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-08-17 17:56:47 -0500

    corrigiendo bd

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,7 @@ const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOO
 const reducers = combineReducers({
     login: loginReducer,
     usuario: registroReducer,
-    uiError: uiReducer.apply,
+    uiError: uiReducer,
     foro:  forumReducer
 })
 //para acciones asincronas 
