05883c00684a131531b584f26ad0c11b5de42977
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-08-18 00:07:44 -0500

    estilos foro

--- src/Components/Forum.jsx
+++ src/Components/Forum.jsx
@@ -30,18 +30,22 @@ export const Forum = () => {
                 <div className="d-flex justify-content-center row">
                     <div className="d-flex flex-column col-md-8">
                         <div className="d-flex flex-row align-items-center text-left comment-top p-2 bg-white border-bottom px-4">
-                            <div className="profile-image"><img className="rounded-circle" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" width="70"></img></div>
+                            <div className="profile-image">
+                                <img className="rounded-circle" src="https://i.ibb.co/9nvRxgM/Logo-proyecto.png" width="70">
+
+                                </img>
+                                </div>
                             <div className="d-flex flex-column-reverse flex-grow-0 align-items-center votings ml-1"></div>
                             <div className="d-flex flex-column ml-4">
                                 <div className="d-flex flex-row post-title">
-                                    <h5>Is sketch 3.9.1 stable?</h5><span className="ml-2">(Jesshead)</span>
+                                    <h5>Cuentanos tu historia?</h5><span className="ml-2"></span>
                                 </div>
                                 <div className="d-flex flex-row align-items-center align-content-center post-title"><span>6 hours ago</span></div>
                             </div>
                         </div>
                         <div className="coment-bottom bg-white p-2 px-4">
-                            <div className="d-flex flex-row add-comment-section mt-4 mb-4">
-                                <img className="img-fluid img-responsive rounded-circle mr-2" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" width="38" alt="imgen" />
+                            <div className="d-flex flex-row add-comment-section mt-2 mb-2">
+                                <img className="img-fluid  rounded-circle mr-2" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" width="38" alt="imgen" />
                                 <Form.Control
                                     type="text"
                                     name="text"
@@ -49,7 +53,7 @@ export const Forum = () => {
                                     value={text}
                                     onChange={handleInputChange}
                                     required />
-                                <button className="btn btn-primary" type="button" onClick={handleNewComment}>Comentar</button>
+                                <button className="btn btn-primary btn-comment"  type="button" onClick={handleNewComment}>Comentar</button>
                             </div>
 
                             <div className="commented-section mt-2">
